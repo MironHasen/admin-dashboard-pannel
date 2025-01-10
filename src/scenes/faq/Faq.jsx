f67d63e6248ff9936552b689a17ad1dae1d47f52
MironHasen <mir0n.hasen@gmail.com>
2025-01-11 00:03:04 +0600

    This is Admin Dashborad Pannel Application

---
+++ src/scenes/faq/Faq.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { Box, useTheme, Typography } from "@mui/material";
+import Header from "../../components/Header";
+import { Accordion } from "@mui/material";
+import { AccordionSummary } from "@mui/material";
+import { AccordionDetails } from "@mui/material";
+import { ExpandMore } from "@mui/icons-material";
+import { tokens } from "../../theme";
+
+const Faq = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Question Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Important Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            What is your Name ?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Favourite Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            What is your Birthday ?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random Question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            What is your first school day ?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMore />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            The Final question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            What is your favourite game ?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
+
+export default Faq;
